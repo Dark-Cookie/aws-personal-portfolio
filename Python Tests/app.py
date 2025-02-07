acba42898f62bd041d03ae8a57a612743131f083
Asif <asif26073@gmail.com>
2025-02-07 12:28:57 +0530

    removed extra files

--- Python Tests/app.py
+++ Python Tests/app.py
@@ -6,7 +6,7 @@ import boto3
 # Setup logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-handler = logging.StreamHandler()  # Console handler
+handler = logging.StreamHandler()
 handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
